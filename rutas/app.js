6c83cb7b8787d6417f316ac57d9ca948e75c7a50
HeberLZ <heberlopez@gmail.com>
2015-03-25 15:27:11 -0300

    cambios de ultimo momento

--- rutas/app.js
+++ rutas/app.js
@@ -1,11 +1,12 @@
 var app = angular.module("manejoDeRutas", ["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider.when("/", {
-        template: 'Página principal<br><a ng-href="#/vista2">Ir a página secundaria</a>'
+        template: '<h1>Página principal</h1>' +
+        '<br><a ng-href="#/vista2">Ir a página secundaria</a>'
     });
     $routeProvider.when("/vista2", {
         templateUrl: 'vista2.html'
-//vista2.html tiene 'Página secundaria<br><a ng-href="#/">Ir a página principal</a>'
+//vista2.html tiene '<h1>Página secundaria</h1><br><a ng-href="#/">Ir a página principal</a>'
     });
     $routeProvider.otherwise({ redirectTo: "/" });
 });
